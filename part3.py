60d1b06131da2f1a272be53ab1bf04de88b6d7a6
Saad T <tariqs26@mcmaster.ca>
2023-04-14 03:27:33 -0400

    Completed part 3: Needs to be reviewed - added return statement of graph and connections in read connections function - call and destructure graph and connections before defining the calc a* function, and the additional functions based on the types of line connections (same, adjacent, multiple transfers) - update calc a* to return the path as well as the distance - create same_lines, adjacent lines and multiple transfers functions - added new lists for the 3 additional plots - added logic to conditionally append to the correct list based on the type of connection - added the 3 additional plots to the main function

--- part3.py
+++ part3.py
@@ -58,46 +58,153 @@ def read_station_connections():
                 graph.add_node(station2)
 
             graph.add_edge(station1, station2, weight)
+            connections[(station1, station2)] = int(line)
+    return graph, connections
 
-stations = read_stations()
 
-def main():
-    graph = DirectedWeightedGraph()
-    read_station_connections(stations, graph)
+graph, connections = read_station_connections()
 
-    nodes = graph.adj.keys()
-    time_d = []
-    time_a = []
-    distance = []
-    for i in nodes:
-        for j in nodes:
-            if i != j:
-                start_time = timeit.default_timer()
-                dists = dijkstra(graph, i)
-                d = dists[j] if j in dists else 0
-                time_duration = timeit.default_timer() - start_time
-                time_d.append(time_duration)
-                start_time = timeit.default_timer()
-                d = calc_sp_a_star(graph, i ,j)
-                time_duration = timeit.default_timer() - start_time
-                time_a.append(time_duration)
-                distance.append(d)
-    
-    plot.title(f"shortest path distance vs. run time\n(unreachable nodes are consider as distance 0)")
-    plot.xlabel("Distance")
-    plot.ylabel("Run Time")
-    plot.scatter(distance, time_d, label = "dijkstra")
-    plot.scatter(distance, time_a, label = "A*")
-    plot.legend()
-    plot.show()
 
-def calc_sp_a_star(graph, source, dest):
-    heuristic = {node: distance(stations[node], stations[dest]) for node in list(graph.adj.keys())}
+def calc_sp_a_star(source, dest):
+    heuristic = {
+        node: distance(stations[node], stations[dest])
+        for node in list(graph.adj.keys())
+    }
     _, path = a_star(graph, source, dest, heuristic)
     dist: float = 0
     for i in range(1, len(path)):
         dist += graph.w(path[i - 1], path[i])
-    return dist
+    return dist, path
+
+
+def same_lines(path: list[int]):
+    if not path:
+        return False
+    if len(path) == 2:
+        return True
+    for i in range(len(path) - 2):
+        if (
+            connections[(path[i], path[i + 1])]
+            != connections[(path[i + 1], path[i + 2])]
+        ):
+            return False
+    return True
+
+
+def adjacent_lines(path: list[int]):
+    if not path or len(path) == 2 or same_lines(path):
+        return False
+    for i in range(len(path) - 2):
+        if (
+            abs(
+                connections[(path[i], path[i + 1])]
+                - connections[(path[i + 1], path[i + 2])]
+            )
+            != 1
+        ):
+            return False
+
+    return True
+
+
+def multiple_line_transfers(path: list[int]):
+    return len(path) > 2 and not same_lines(path) and not adjacent_lines(path)
+
+
+def main():
+    nodes = graph.adj.keys()
+    time_d = []
+    time_a = []
+    distance = []
+
+    time_a_same_line = []
+    time_d_same_line = []
+    distance_same_line = []
+
+    time_a_adjacent_line = []
+    time_d_adjacent_line = []
+    distance_adjacent_line = []
+
+    time_a_multiple_line_transfer = []
+    time_d_multiple_line_transfer = []
+    distance_multiple_line_transfer = []
+
+    for i in nodes:
+        for j in nodes:
+            if i != j:
+                start_time = timeit.default_timer()
+                d, path = calc_sp_a_star(i, j)
+                time_duration = timeit.default_timer() - start_time
+                time_a.append(time_duration)
+
+                if same_lines(path):
+                    time_a_same_line.append(time_duration)
+                    distance_same_line.append(d)
+                elif adjacent_lines(path):
+                    time_a_adjacent_line.append(time_duration)
+                    distance_adjacent_line.append(d)
+                elif multiple_line_transfers(path):
+                    time_a_multiple_line_transfer.append(time_duration)
+                    distance_multiple_line_transfer.append(d)
+
+                start_time = timeit.default_timer()
+                dijkstra(graph, i)
+                time_duration = timeit.default_timer() - start_time
+                time_d.append(time_duration)
+
+                if same_lines(path):
+                    time_d_same_line.append(time_duration)
+                elif adjacent_lines(path):
+                    time_d_adjacent_line.append(time_duration)
+                elif multiple_line_transfers(path):
+                    time_d_multiple_line_transfer.append(time_duration)
+
+                distance.append(d)
+
+    plot.title(
+        f"Shortest path distance vs. run time\n(unreachable nodes are consider as distance 0)"
+    )
+    plot.xlabel("Distance")
+    plot.ylabel("Run Time")
+    plot.scatter(distance, time_d, label="Dijkstra")
+    plot.scatter(distance, time_a, label="A*")
+    plot.legend()
+    plot.show()
+
+    plot.title(
+        f"Shortest path distance vs. run time for same line connections\n(unreachable nodes are consider as distance 0)"
+    )
+    plot.xlabel("Distance")
+    plot.ylabel("Run Time")
+    plot.scatter(distance_same_line, time_d_same_line, label="Dijkstra")
+    plot.scatter(distance_same_line, time_a_same_line, label="A*")
+    plot.legend()
+    plot.show()
+
+    plot.title(
+        f"Shortest path distance vs. run time for adjacent line connections\n(unreachable nodes are consider as distance 0)"
+    )
+    plot.xlabel("Distance")
+    plot.ylabel("Run Time")
+    plot.scatter(distance_adjacent_line, time_d_adjacent_line, label="Dijkstra")
+    plot.scatter(distance_adjacent_line, time_a_adjacent_line, label="A*")
+    plot.legend()
+    plot.show()
+
+    plot.title(
+        f"Shortest path distance vs. run time for multiple line transfers\n(unreachable nodes are consider as distance 0)"
+    )
+    plot.xlabel("Distance")
+    plot.ylabel("Run Time")
+    plot.scatter(
+        distance_multiple_line_transfer, time_d_multiple_line_transfer, label="Dijkstra"
+    )
+    plot.scatter(
+        distance_multiple_line_transfer, time_a_multiple_line_transfer, label="A*"
+    )
+    plot.legend()
+    plot.show()
+
 
 if __name__ == "__main__":
     main()
