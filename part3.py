1249c12cdcaefc472eb83f7698303c493b6cd282
Zhuo Chen <chenz327@mcmaster.ca>
2023-04-13 23:44:08 -0400

    compare of A8 and Dijkstra

--- part3.py
+++ part3.py
@@ -1,6 +1,8 @@
 import csv
-
-from final_project_part1 import DirectedWeightedGraph
+from exp2 import a_star
+from final_project_part1 import DirectedWeightedGraph, dijkstra
+import matplotlib.pyplot as plot
+import timeit
 
 """
 type: station row
@@ -14,7 +16,6 @@ station1, station2
 
 stations_file = "./csv_files/london_stations.csv"
 station_connections_file = "./csv_files/london_connections.csv"
-
 
 def distance(station1, station2):
     """
@@ -32,7 +33,7 @@ def read_stations():
         reader = csv.reader(f)
         next(reader)  # skip header
         for id, long, lat, *_ in reader:
-            stations[id] = (float(lat), float(long))
+            stations[int(id)] = (float(lat), float(long))
     return stations
 
 
@@ -41,6 +42,8 @@ def read_station_connections(stations, graph):
         reader = csv.reader(f)
         next(reader)  # skip header
         for station1, station2, *_ in reader:
+            station1 = int(station1)
+            station2 = int(station2)
             weight = distance(stations[station1], stations[station2])
 
             if station1 not in graph.adj:
@@ -50,13 +53,45 @@ def read_station_connections(stations, graph):
 
             graph.add_edge(station1, station2, weight)
 
+stations = read_stations()
 
 def main():
-    stations = read_stations()
     graph = DirectedWeightedGraph()
     read_station_connections(stations, graph)
-    print(graph.adj)
 
+    nodes = graph.adj.keys()
+    time_d = []
+    time_a = []
+    distance = []
+    for i in nodes:
+        for j in nodes:
+            if i != j:
+                start_time = timeit.default_timer()
+                dists = dijkstra(graph, i)
+                d = dists[j] if j in dists else 0
+                time_duration = timeit.default_timer() - start_time
+                time_d.append(time_duration)
+                start_time = timeit.default_timer()
+                d = calc_sp_a_star(graph, i ,j)
+                time_duration = timeit.default_timer() - start_time
+                time_a.append(time_duration)
+                distance.append(d)
+    
+    plot.title(f"shortest path distance vs. run time\n(unreachable nodes are consider as distance 0)")
+    plot.xlabel("Distance")
+    plot.ylabel("Run Time")
+    plot.scatter(distance, time_d, label = "dijkstra")
+    plot.scatter(distance, time_a, label = "A*")
+    plot.legend()
+    plot.show()
+
+def calc_sp_a_star(graph, source, dest):
+    heuristic = {node: distance(stations[node], stations[dest]) for node in list(graph.adj.keys())}
+    _, path = a_star(graph, source, dest, heuristic)
+    dist: float = 0
+    for i in range(1, len(path)):
+        dist += graph.w(path[i - 1], path[i])
+    return dist
 
 if __name__ == "__main__":
     main()
