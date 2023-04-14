82553e35a017e6cbacc2a55333d3cd989b64e234
Zhuo Chen <chenz327@mcmaster.ca>
2023-04-13 21:32:36 -0400

    comments for A*

--- exp2.py
+++ exp2.py
@@ -2,7 +2,21 @@ from typing import Callable
 from final_project_part1 import DirectedWeightedGraph
 from min_heap import MinHeap, Element
 
-def a_star(G: DirectedWeightedGraph, source: int, des: int, heuristic: Callable[[int], float]):
+def a_star(G: DirectedWeightedGraph, source: int, des: int, heuristic: Callable[[int], float]) -> tuple[dict, list]:
+    """
+    Computes the shortest path between a source and destination node in a weighted directed graph using the A* algorithm.
+    
+    Args:
+        G (DirectedWeightedGraph): The graph to search for the shortest path.
+        source (int): The node to start the search from.
+        des (int): The destination node to search for.
+        heuristic (Callable[[int], float]): A function that estimates the distance between a node and the destination node.
+        
+    Returns:
+        A tuple of two elements:
+        1. A dictionary that maps each node to its predecessor in the shortest path from the source node to that node.
+        2. A list of the nodes in the shortest path from the source node to the destination node.
+    """
     pred = {}
     Q = MinHeap([])
     Q.insert(Element(source, 0))
