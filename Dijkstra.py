04bfd35d367130628a16dfaecfee5cfb8d53e4a8
Saad T <tariqs26@mcmaster.ca>
2023-04-15 20:30:08 -0400

    refactor Dijkstra to a class

---
+++ Dijkstra.py
@@ -0,0 +1,37 @@
+from SPAlgorithm import *
+from min_heap import *
+
+
+class Dijkstra(SPAlgorithm):
+    def calc_sp(graph: Graph, source: int) -> float:
+        pred = (
+            {}
+        )  # Predecessor dictionary. Isn't returned, but here for your understanding
+        dist = {}  # Distance dictionary
+        Q = MinHeap([])
+        nodes = Graph.get_nodes()
+
+        # Initialize priority queue/heap and distances
+        for node in nodes:
+            Q.insert(Element(node, float("inf")))
+            dist[node] = float("inf")
+        Q.decrease_key(source, 0)
+
+        # Meat of the algorithm
+        while not Q.is_empty():
+            current_element = Q.extract_min()
+            current_node = current_element.value
+            dist[current_node] = current_element.key
+            for neighbour in Graph.get_adj_nodes(current_node):
+                if (
+                    dist[current_node] + Graph.w(current_node, neighbour)
+                    < dist[neighbour]
+                ):
+                    Q.decrease_key(
+                        neighbour, dist[current_node] + Graph.w(current_node, neighbour)
+                    )
+                    dist[neighbour] = dist[current_node] + Graph.w(
+                        current_node, neighbour
+                    )
+                    pred[neighbour] = current_node
+        return dist
